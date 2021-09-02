949707f2237e948cee3d2709580e933ed59f94be
klyskawa <lyskawakrz@gmail.com>
2021-09-02 18:28:39 +0200

    Fixed the ugliness

--- SnakeController/SnakeController.cpp
+++ SnakeController/SnakeController.cpp
@@ -221,26 +221,17 @@ void Controller::handlePause(const PauseInd&)
 
 void Controller::receive(std::unique_ptr<Event> e)
 {
-    if(paused)
-    {
-        switch(e->getMessageId())
-        {            
-            case FoodInd::MESSAGE_ID: return handleFoodPositionChange(*static_cast<EventT<FoodInd> const&>(*e));
-            case FoodResp::MESSAGE_ID: return handleNewFood(*static_cast<EventT<FoodResp> const&>(*e));
-            case PauseInd::MESSAGE_ID: return handlePause(*static_cast<EventT<PauseInd> const&>(*e));
-        }
-    }
-    else{
+    
         switch(e->getMessageId())
         {
-            case TimeoutInd::MESSAGE_ID: return handleTimePassed(*static_cast<EventT<TimeoutInd> const&>(*e));
-            case DirectionInd::MESSAGE_ID: return handleDirectionChange(*static_cast<EventT<DirectionInd> const&>(*e));
+            case TimeoutInd::MESSAGE_ID: if(!paused) {handleTimePassed(*static_cast<EventT<TimeoutInd> const&>(*e));} return;
+            case DirectionInd::MESSAGE_ID: if(!paused) {handleDirectionChange(*static_cast<EventT<DirectionInd> const&>(*e));} return;
             case FoodInd::MESSAGE_ID: return handleFoodPositionChange(*static_cast<EventT<FoodInd> const&>(*e));
             case FoodResp::MESSAGE_ID: return handleNewFood(*static_cast<EventT<FoodResp> const&>(*e));
             case PauseInd::MESSAGE_ID: return handlePause(*static_cast<EventT<PauseInd> const&>(*e));
             default: throw UnexpectedEventException();
         };
-    }
+    
 }
 
 } // namespace Snake
