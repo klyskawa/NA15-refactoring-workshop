bedc64fb909edd2009505f276a7416f3b6507588
klyskawa <lyskawakrz@gmail.com>
2021-09-02 18:24:10 +0200

    Added pause function, tests green but a bit ugly

--- SnakeController/SnakeController.cpp
+++ SnakeController/SnakeController.cpp
@@ -213,16 +213,34 @@ Controller::Segment Controller::getNewHead() const
     return newHead;
 }
 
+
+void Controller::handlePause(const PauseInd&)
+{
+    paused = !paused;
+}
+
 void Controller::receive(std::unique_ptr<Event> e)
 {
-    switch(e->getMessageId())
+    if(paused)
     {
-        case TimeoutInd::MESSAGE_ID: return handleTimePassed(*static_cast<EventT<TimeoutInd> const&>(*e));
-        case DirectionInd::MESSAGE_ID: return handleDirectionChange(*static_cast<EventT<DirectionInd> const&>(*e));
-        case FoodInd::MESSAGE_ID: return handleFoodPositionChange(*static_cast<EventT<FoodInd> const&>(*e));
-        case FoodResp::MESSAGE_ID: return handleNewFood(*static_cast<EventT<FoodResp> const&>(*e));
-        default: throw UnexpectedEventException();
-    };
+        switch(e->getMessageId())
+        {            
+            case FoodInd::MESSAGE_ID: return handleFoodPositionChange(*static_cast<EventT<FoodInd> const&>(*e));
+            case FoodResp::MESSAGE_ID: return handleNewFood(*static_cast<EventT<FoodResp> const&>(*e));
+            case PauseInd::MESSAGE_ID: return handlePause(*static_cast<EventT<PauseInd> const&>(*e));
+        }
+    }
+    else{
+        switch(e->getMessageId())
+        {
+            case TimeoutInd::MESSAGE_ID: return handleTimePassed(*static_cast<EventT<TimeoutInd> const&>(*e));
+            case DirectionInd::MESSAGE_ID: return handleDirectionChange(*static_cast<EventT<DirectionInd> const&>(*e));
+            case FoodInd::MESSAGE_ID: return handleFoodPositionChange(*static_cast<EventT<FoodInd> const&>(*e));
+            case FoodResp::MESSAGE_ID: return handleNewFood(*static_cast<EventT<FoodResp> const&>(*e));
+            case PauseInd::MESSAGE_ID: return handlePause(*static_cast<EventT<PauseInd> const&>(*e));
+            default: throw UnexpectedEventException();
+        };
+    }
 }
 
 } // namespace Snake
